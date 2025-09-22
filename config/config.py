7036ef4b0b97fbe1fda091cd3082bc97e1637397
Klick-gif <shengquan0011@gmail.com>
2025-09-22 16:57:10 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -4,10 +4,11 @@ from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(
     model="qwen3-235b-a22b-instruct-2507",
     base_url='https://dashscope.aliyuncs.com/compatible-mode/v1',
-    api_key="sk-eb8efc4f031c464c8db80722d7c7dbd1",
+    api_key="Your-API-KEY",
     streaming=True
 )
 
 # 创建本地Qwen模型，需要在本地显存高的情况下使用
 # local_llm = create_local_qwen_model()
+
 
